889e458c369433f021af20e26be0ec28ac630b94
altugaltay <altayaltug1997@gmail.com>
2022-09-15 10:03:52 +0300

    updates

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ async function filterByGroupName(groupName) {
 
     const dataJson = await fetch("./data.json")  // pull data.json
         .then((response) => response.json())
-        .catch((error) => console.log(error));  // if occurs some error, show error.
-        debugger
+        .catch((err) => console.log(err));  // if occurs some error, show error.
+        //debugger
 
         let dataList = dataJson.filter(item => item.group === groupName); // filtering process
-        debugger
+        //debugger
         // control block 10-17
         if(dataList.length>1)
         {
@@ -19,24 +19,25 @@ async function filterByGroupName(groupName) {
 };
 
 function getGroups(array){
-    const result = array.reduce((total, currentValue) => {   
+    let result = array.reduce((total, currentValue) => {   
                          
         if (currentValue.type == null) // collect student data
         {    
 
             total[currentValue.type] = total[currentValue.type] || [];
             total[currentValue.type].push(currentValue.name); // push student names into array
-            debugger
+            //debugger
         } 
         else  // collect assistant data
         {                                                                
             total[currentValue.type] = total[currentValue.type] || [];
             total[currentValue.type].push(currentValue.name); // push assistant name into array
-            debugger
+            //debugger
         }
         return total;
-    }, Object.create(null)
-    )
+    }, {}
+    );
+
     console.log(result);                   
 }
 
